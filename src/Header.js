0a09d7a4330b9a2189de79fb1de9f9ecc9ad4422
Meet Shukla <meet22599@gmail.com>
2022-01-07 23:35:59 +0530

    07-01-2021 work

--- src/Header.js
+++ src/Header.js
@@ -16,8 +16,8 @@ function header() {
       </div>
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello</span>
-          <span className="header__optionLineTwo">User</span>
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
