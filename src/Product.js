eaefbdafebb4cf1dccefe7bc16004d9fd51d1818
Meet Shukla <meet22599@gmail.com>
2022-01-18 00:23:17 +0530

    18-01

--- src/Product.js
+++ src/Product.js
@@ -14,7 +14,7 @@ function Product({ id, title, image, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p></p>
+              <p>🌟</p>
             ))}
         </div>
       </div>
